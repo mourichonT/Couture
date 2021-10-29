4d10554c514a9616bb9b24f0c687aedbc30a3d09
mourichonT <47600724+mourichonT@users.noreply.github.com>
2021-10-29 18:59:06 +0200

    work of friday

--- js/carousselrea.js
+++ js/carousselrea.js
@@ -1,56 +1,161 @@
-function moveToSelected(element) {
-    let selected = "";
-    if (element == "next") {
-        selected = $(".selected").next();
-    } else if (element == "prev") {
-        selected = $(".selected").prev();
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+class Carousel {
+  constructor(container, items, controls) {
+    this.carouselContainer = container;
+    this.carouselControls = controls;
+    this.carouselArray = [...items];
+  }
+
+  // Update css classes for gallery
+  updateGallery() {
+    this.carouselArray.forEach(el => {
+      el.classList.remove('gallery-item-1');
+      el.classList.remove('gallery-item-2');
+      el.classList.remove('gallery-item-3');
+      el.classList.remove('gallery-item-4');
+      el.classList.remove('gallery-item-5');
+    });
+
+    this.carouselArray.slice(0, 5).forEach((el, i) => {
+      el.classList.add(`gallery-item-${i+1}`);
+    });
+  }
+
+  // Update the current order of the carouselArray and gallery
+  setCurrentState(direction) {
+
+    if (direction.className == 'gallery-controls-previous') {
+      this.carouselArray.unshift(this.carouselArray.pop());
     } else {
-        selected = element;
+      this.carouselArray.push(this.carouselArray.shift());
     }
+    
+    this.updateGallery();
+  }
 
-    var next = $(selected).next();
-    var prev = $(selected).prev();
-    var prevSecond = $(prev).prev();
-    var nextSecond = $(next).next();
+  // Construct the carousel navigation
+  // setNav() {
+    // galleryContainer.appendChild(document.createElement('ul')).className = 'gallery-nav';
 
-    $(selected).removeClass().addClass("border_radius selected");
+    // this.carouselArray.forEach(item => {
+    //   const nav = galleryContainer.lastElementChild;
+    //   nav.appendChild(document.createElement('li'));
+    // }); 
+  // }s
 
-    $(prev).removeClass().addClass("border_radius prev");
-    $(next).removeClass().addClass("border_radius next");
+  // Construct the carousel controls
+  setControls() {
+    this.carouselControls.forEach(control => {
+      galleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
 
-    $(nextSecond).removeClass().addClass("border_radius nextRightSecond");
-    $(prevSecond).removeClass().addClass("border_radius prevLeftSecond");
+      document.querySelector(`.gallery-controls-${control}`).innerText = control;
+    });
+  }
+ 
+  // Add a click event listener to trigger setCurrentState method to rearrange carousel
+  useControls() {
+    const triggers = [...galleryControlsContainer.childNodes];
 
-    $(nextSecond).nextAll().removeClass().addClass('border_radius hideRight');
-    $(prevSecond).prevAll().removeClass().addClass('border_radius hideLeft');
+    triggers.forEach(control => {
+      control.addEventListener('click', e => {
+        e.preventDefault();
 
+        if (control.className == 'gallery-controls-add') {
+          const newItem = document.createElement('img');
+          const latestItem = this.carouselArray.length;
+          const latestIndex = this.carouselArray.findIndex(item => item.getAttribute('data-index') == this.carouselArray.length)+1;
+
+          // Assign the necessary properties for new gallery item
+          Object.assign(newItem,{
+            className: 'gallery-item',
+            src: `http://fakeimg.pl/300/?text=${this.carouselArray.length+1}`
+          });
+          newItem.setAttribute('data-index', this.carouselArray.length+1);
+
+          // Then add it to the carouselArray and update the gallery
+          this.carouselArray.splice(latestIndex, 0, newItem);
+          document.querySelector(`[data-index="${latestItem}"]`).after(newItem);
+          this.updateGallery();
+
+        } else {
+          this.setCurrentState(control);
+        }
+
+      });
+    });
+  }
 }
 
-// Eventos teclado
-$(document).keydown(function(e) {
-    switch (e.which) {
-        case 37: // left
-            moveToSelected('prev');
-            break;
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
 
-        case 39: // right
-            moveToSelected('next');
-            break;
+exampleCarousel.setControls();
+// exampleCarousel.setNav();
+exampleCarousel.useControls();
 
-        default:
-            return;
-    }
-    e.preventDefault();
-});
 
-$('#carousel div').click(function() {
-    moveToSelected($(this));
-});
 
-$('#prev').click(function() {
-    moveToSelected('prev');
-});
 
-$('#next').click(function() {
-    moveToSelected('next');
-});
+
+
+
+
+// function moveToSelected(element) {
+//     let selected = "";
+//     if (element == "next") {
+//         selected = $(".selected").next();
+//     } else if (element == "prev") {
+//         selected = $(".selected").prev();
+//     } else {
+//         selected = element;
+//     }
+
+//     var next = $(selected).next();
+//     var prev = $(selected).prev();
+//     var prevSecond = $(prev).prev();
+//     var nextSecond = $(next).next();
+
+//     $(selected).removeClass().addClass("border_radius selected");
+
+//     $(prev).removeClass().addClass("border_radius prev");
+//     $(next).removeClass().addClass("border_radius next");
+
+//     $(nextSecond).removeClass().addClass("border_radius nextRightSecond");
+//     $(prevSecond).removeClass().addClass("border_radius prevLeftSecond");
+
+//     $(nextSecond).nextAll().removeClass().addClass('border_radius hideRight');
+//     $(prevSecond).prevAll().removeClass().addClass('border_radius hideLeft');
+
+// }
+
+// // Eventos teclado
+// $(document).keydown(function(e) {
+//     switch (e.which) {
+//         case 37: // left
+//             moveToSelected('prev');
+//             break;
+
+//         case 39: // right
+//             moveToSelected('next');
+//             break;
+
+//         default:
+//             return;
+//     }
+//     e.preventDefault();
+// });
+
+// $('#carousel div').click(function() {
+//     moveToSelected($(this));
+// });
+
+// $('#prev').click(function() {
+//     moveToSelected('prev');
+// });
+
+// $('#next').click(function() {
+//     moveToSelected('next');
+// });
